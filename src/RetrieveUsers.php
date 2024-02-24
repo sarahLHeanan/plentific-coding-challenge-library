43e50740d559189d2c883fa5457d7c436266983f
Sarah Heanan <sarahlheanan@gmail.com>
2024-02-24 20:57:58 +0000

    Bootstrap Retrieve Users class - set up methods and docblocks, working example

--- src/RetrieveUsers.php
+++ src/RetrieveUsers.php
@@ -1,10 +1,60 @@
 <?php
+
 class RetrieveUsers
 {
     public $package = 'the package is loading';
+    public $client;
+    const API_URL = 'https://reqres.in/';
+    
 
     public function testPackage() {
         echo $this->package;
     }
+
+    public function __construct() {
+        $this->client = new \GuzzleHttp\Client();
+    }
+    
+
+    /**
+     * Get User By Id
+     * @param $id The user id
+     *
+     * @return User $user
+     * @todo functionality
+     * @todo validation
+     */
+    public function getUser(int $id)
+    {
+
+    }
+
+    /**
+     * Get list of users
+     *
+     * @return Array
+     * @todo basic functionality
+     * @todo pagination
+     * @todo validation/error handling
+     */
+    public function getUsers()
+    {
+        $response = $this->client->request('GET', self::API_URL);
+
+        var_dump($response);
+    }
+
+    /**
+     * Create a user
+     *
+     * @return void
+     * @todo functionality
+     * @todo validation/error handling
+     */
+    public function createUser()
+    {
+
+    }
 }
+
 ?>
