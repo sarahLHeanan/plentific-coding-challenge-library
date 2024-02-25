148affb7b462a7b8d8ba63f5c0bb0a541b269765
Sarah Heanan <sarahlheanan@gmail.com>
2024-02-25 20:33:04 +0000

    Outline User test and methods

---
+++ tests/UserTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class UserTest extends TestCase
+{
+    public function testUserReturnsSingleUser(): void
+    {
+
+    }
+
+    public function testUserReturnsListOfUsers(): void
+    {
+
+    }
+
+    public function testUserListHasPagination(): void
+    {
+
+    }
+
+    public function testUserCanCreateAUser(): void
+    {
+
+    }
+
+    public function testExceptionIsThrownIfDataIsUnavailable(): void
+    {
+
+    }
+
+    public function testExceptionIsThrownIfDataHasChanged(): void
+    {
+
+    }
+
+    public function testExceptionThrownIsSpecificToDomain(): void
+    {
+
+    }
+}
